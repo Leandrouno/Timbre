b9c2d2e98000f2252b096d51a16dbf8740f3abc3
Leandrouno <lmugnolo@gmail.com>
2018-07-20 21:54:34 -0300

    Update Timbre.ino

--- Timbre.ino
+++ Timbre.ino
@@ -1,92 +1,82 @@
-// Incluyo Librerias
 #include <ESP8266WiFi.h>
 #include <WiFiClientSecure.h>
-#include <TelegramBot.h>
-// Defino Pines
-#define BOTON_PIN D5
-#define LED_BUILTIN D2
+#include <UniversalTelegramBot.h>
+// Inicializamos la conexion WIFI con el Router
+char ssid[] = "xxxxxxx";     // el nombre de tu Red
+char password[] = "xxxxxxx"; // la clave de tu Red
+const char  id_chat[]="xxxxxx";
+const char respuesta[] = "Timbre" , iniciado[]="Sistema Iniciado....";
+int BanderaInicio=0, BanderaBoton=0;
 volatile bool BanderaBotonPresionado = false;
 
-// Inicializa la conexión Wifi al router
-const char* ssid  = "XXXXXXXXXXX"; // Nombre de la RED WIFI
-const char* password = "XXXXXXXX"; // Contraseña de la Red Wifi
-const char  id_chat[]="XXXXX"; // ID del Chat de Telegram
-// Inicializa Telegram BOT
-const char BotToken[] = "XXXXXXXX:YYYYYYYYYYYYYYYYYY"; // BotToken de Telegram
-// Respuesta
-const char respuesta[] = "Timbre" , iniciado[]="Sistema Iniciado....";
+// Initialize Telegram BOT
+#define BOTtoken "xxxxxxx:xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"  // el token de tu BOT, lo obtenemos de BotFather
 
-
-WiFiClientSecure net_ssl;
-TelegramBot bot (BotToken, net_ssl);
-int BanderaInicio=0, BanderaBoton=0;
+#define BOTON_PIN D1
+#define LED_BUILTIN D2
+WiFiClientSecure client;
+UniversalTelegramBot bot(BOTtoken, client);
 
 void setup() {
+        Serial.begin(9600);
+        // Establecer el modo WiFi y desconectarse de un AP si fue Anteriormente conectada
+        WiFi.mode(WIFI_STA);
+        WiFi.disconnect();
+        delay(100);
+        // Intentar conectarse a la red
+        Serial.print("Conectando a la Red Wifi: ");
+        Serial.println(ssid);
+        WiFi.begin(ssid, password);        
+        while (WiFi.status() != WL_CONNECTED) {Serial.print("."); delay(500);}        
+        Serial.println("");
+        Serial.println("WiFi conectada");   
+                Serial.print("IP address: ");
+        Serial.println(WiFi.localIP());    
+        pinMode(BOTON_PIN, INPUT_PULLUP);
+        pinMode(LED_BUILTIN, OUTPUT);
+        attachInterrupt(BOTON_PIN, BotonPresionado, FALLING);
+      } // Fin Setup
 
-  pinMode(LED_BUILTIN, OUTPUT);
-  Serial.begin(115200);
-  while (!Serial); // Espera a que el Monitor Serial este abierto
 
-  // Intenta conectarse a la red Wifi
-  Serial.print("Connectando Wifi: ");
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-                                          delay(500);
-                                          Serial.print(".");
-                                        }
-                                        
-  Serial.println("");
-  Serial.println("WiFi connectado");
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
-  bot.begin();
-  pinMode(BOTON_PIN, INPUT);
-  attachInterrupt(BOTON_PIN, BotonPresionado, RISING);
-
-  Serial.println(" ");
- 
-
-} // FIN SETUP
+                      
+void loop() { 
+              if (BanderaInicio == 0){ mensaje_inicio ();}
+              if ( BanderaBotonPresionado ) {  envio_mensaje ();} 
+              if (WiFi.status() == WL_CONNECTED) {
+                            digitalWrite(LED_BUILTIN, HIGH);
+                                        } else {digitalWrite(LED_BUILTIN, LOW);}
+              }// FIN LOOP
 
 void BotonPresionado() {    
                         int button = digitalRead(BOTON_PIN);
-                        if(button == HIGH && BanderaBoton == 0)
+                        if(button == LOW && BanderaBoton == 0)
                           {
                           BanderaBotonPresionado = true; 
                           Serial.println("Boton Presionado");
                           BanderaBoton = 1;
                           }                                                    
                         return;
-                       } // FIN  Boton Presionado
-                      
-void loop() { 
-			        if(BanderaInicio == 0){ mensaje_inicio ();}
-              if ( BanderaBotonPresionado ) {  envio_mensaje ();} 
-              if (WiFi.status() == WL_CONNECTED) {
-                            digitalWrite(LED_BUILTIN, HIGH);
-                                        } else {digitalWrite(LED_BUILTIN, LOW);}
-              }// FIN LOOP
-			  
-			  
+                       } // FIN  Boton Presionado        
+        
 void mensaje_inicio (){
-	
-						  Serial.println("Iniciado...");
-                          Serial.println("Enviando Mensaje de Inicio....."); 
+  
+              Serial.println("Iniciado...");
+                          Serial.println("Enviando Mensaje de Inicio ....."); 
                           bot.sendMessage(id_chat, iniciado);
                           Serial.println("Mensaje Enviado"); 
                           Serial.println(" "); 
                           BanderaInicio=1;
                           
-	
+  
 } // FIN Mensaje de inicio
-			  
+        
 void envio_mensaje (){
-	
-						Serial.println("Enviando Mensaje.....");  
+  
+            Serial.println("Enviando Mensaje.....");  
                         BanderaBotonPresionado = false;
                         bot.sendMessage(id_chat, respuesta);
                         Serial.println("Mensaje Enviado");
                         Serial.println(" "); 
                         BanderaBoton = 0;
                                          
-}	// FIN Envio de Mensaje		 
+} // FIN Envio de Mensaje    		 
